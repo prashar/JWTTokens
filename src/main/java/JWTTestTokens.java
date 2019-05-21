cb57832a2307ef3ba2442815f5012b7fe66544b9
Neeraj Prashar <nprashar@amazon.com>
2019-05-20 17:19:22 -0700

    Add README and make code match

--- src/main/java/JWTTestTokens.java
+++ src/main/java/JWTTestTokens.java
@@ -2,7 +2,6 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
-
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
@@ -10,9 +9,7 @@ import java.util.HashMap;
 public class JWTTestTokens {
     public static void main(String[] args) {
         Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-        System.out.println(System.currentTimeMillis() + 1000000);
         Date exp = new Date(System.currentTimeMillis() + 1000000);
-        System.out.println(exp.toString());
         HashMap<String,Object> hm = new HashMap<>();
         hm.put("roles","admin");
         String jws = Jwts.builder()
@@ -21,13 +18,13 @@ public class JWTTestTokens {
                 .setSubject("admin")
                 .setExpiration(exp)
                 .signWith(key).compact();
+        System.out.println("Token:");
         System.out.println(jws);
         if(Jwts.parser().setSigningKey(key).parseClaimsJws(jws).getBody().getSubject().equals("test")) {
             System.out.println("test");
         }
         String encoded = Encoders.BASE64.encode(key.getEncoded());
+        System.out.println("Shared secret:");
         System.out.println(encoded);
-
-
     }
 }
